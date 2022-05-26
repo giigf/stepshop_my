1aa720ea489adff33037ece75e9903c7fe127227
Proshin Aleksei <leshaburunday@gmail.com>
2022-05-26 16:59:21 +0600

    First commit

---
+++ mainapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def products(request):
+    return render(request, 'products.html')
+
+
+def product(request):
+    return render(request, 'single-product.html')
